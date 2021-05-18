6468347580fb156f3185510b3a92f41254c58eb3
kakamal <kamal.kamal@soprasteria.com>
2021-05-18 13:41:33 +0530

    for Devlop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          This code is from develop branch 
         </p>
         <a
           className="App-link"
